81247a02d04b7c89bec585d719b153a1277aca3b
Ryo Shimizu <ryo@nerv.org>
2021-10-19 02:02:04 +0900

    init

---
+++ test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/queue.h>
+
+#include "cyclefind.h"
+
+struct element {
+	int id;
+	struct element *next;
+};
+
+struct element *listhead;
+
+CYCLEFIND_DECL(, struct element *) cf;
+
+int
+main(int argc, char *argv[])
+{
+#define N	1000
+	struct element elm[N];
+	struct element *e;
+	int i;
+
+	memset(elm, 0, sizeof(elm));
+
+	/* insert head */
+	for (i = 0; i < __arraycount(elm); i++) {
+		elm[i].id = i;
+		elm[i].next = listhead;
+		listhead = &elm[i];
+	}
+
+	/* make loop */
+	elm[123].next = &elm[456];
+
+	/* cycle detection test */
+	CYCLEFIND_INIT(&cf, 10000);
+	for (e = listhead; e != NULL; e = e->next) {
+		int n;
+		if ((n = CYCLEFIND_TEST(&cf, e)) != 0) {
+			printf("Looped! last %d link looped\n", n);
+			break;
+		}
+
+		printf("%p: %d\n", e, e->id);
+	}
+}
